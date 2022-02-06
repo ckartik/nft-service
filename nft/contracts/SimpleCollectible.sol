55668e6ee49b56a9464c734c33818b693f985378
ckartik <k5chopra@uwaterloo.ca>
2022-02-06 01:26:22 -0500

    Updates NFT to take in new address.

--- nft/contracts/SimpleCollectible.sol
+++ nft/contracts/SimpleCollectible.sol
@@ -5,13 +5,13 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCollectible is ERC721 {
     uint256 public tokenCounter;
-    constructor () public ERC721 ("Dogie", "DOG"){
+    constructor () public ERC721 ("NFT Bridge", "NFT"){
         tokenCounter = 0;
     }
 
-    function createCollectible(string memory tokenURI) public returns (uint256) {
+    function createCollectible(address newAddress, string memory tokenURI) public returns (uint256) {
         uint256 newItemId = tokenCounter;
-        _safeMint(msg.sender, newItemId);
+        _safeMint(newAddress, newItemId);
         _setTokenURI(newItemId, tokenURI);
         tokenCounter = tokenCounter + 1;
         return newItemId;
